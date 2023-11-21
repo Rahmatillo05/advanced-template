f77a6b72864c2c3ac3f88199efe0541c8fdd787c
Rahmatillo05 <hrrahmatillo2oo5@gmail.com>
2023-11-21 07:52:43 +0500

    fixed bugs

--- console/controllers/InitController.php
+++ console/controllers/InitController.php
@@ -3,7 +3,7 @@
 namespace console\controllers;
 
 use yii\console\Controller;
-use yii\helpers\Console;
+use yii\helpers\BaseConsole;
 
 class InitController extends Controller
 {
@@ -14,15 +14,15 @@ class InitController extends Controller
         $env_example_path = __DIR__ . '/../../.env.example';
         if (!file_exists($env_path)) {
             copy($env_example_path, $env_path);
-            $this->stdout("File .env created.\n", Console::FG_GREEN);
+            $this->stdout("File .env created.\n", BaseConsole::FG_GREEN);
         } else{
-            $this->stdout("File .env already exists.\n", Console::FG_YELLOW);
+            $this->stdout("File .env already exists.\n", BaseConsole::FG_YELLOW);
         }
 
         $bash_query = "php yii migrate --interactive=0 --migrationPath=@frontend/modules/file/migrations";
         $output = shell_exec($bash_query);
-        $this->stdout("Running: $bash_query\n", Console::FG_YELLOW);
-        $this->stdout($output, Console::FG_GREEN);
+        $this->stdout("Running: $bash_query\n", BaseConsole::FG_YELLOW);
+        $this->stdout($output, BaseConsole::FG_GREEN);
     }
 
 }
