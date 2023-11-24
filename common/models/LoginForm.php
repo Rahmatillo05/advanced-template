8d934f87d4b57b0be87d6cf3264ae004620cf3f6
Rahmatillo05 <hrrahmatillo2oo5@gmail.com>
2023-11-25 00:26:37 +0500

    user auth, roles, generate and set token

--- common/models/LoginForm.php
+++ common/models/LoginForm.php
@@ -15,6 +15,8 @@ class LoginForm extends Model
     public $username;
     public $password;
 
+    private $_user;
+
 
     /**
      * {@inheritdoc}
@@ -27,7 +29,14 @@ class LoginForm extends Model
         ];
     }
 
-    public function validatePassword($attribute, $params): void
+    /**
+     * Validates the password.
+     * This method serves as the inline validation for password.
+     *
+     * @param string $attribute the attribute currently being validated
+     * @param array $params the additional name-value pairs given in the rule
+     */
+    public function validatePassword($attribute, $params)
     {
         if (!$this->hasErrors()) {
             $user = $this->getUser();
@@ -35,5 +44,33 @@ class LoginForm extends Model
                 $this->addError($attribute, 'Incorrect username or password.');
             }
         }
+    }
+
+    /**
+     * Logs in a user using the provided username and password.
+     *
+     * @return bool whether the user is logged in successfully
+     */
+    public function login()
+    {
+        if ($this->validate()) {
+            return Yii::$app->user->login($this->getUser(), $this->rememberMe ? 3600 * 24 * 30 : 0);
+        }
+
+        return false;
+    }
+
+    /**
+     * Finds user by [[username]]
+     *
+     * @return User|null
+     */
+    protected function getUser()
+    {
+        if ($this->_user === null) {
+            $this->_user = User::findByUsername($this->username);
+        }
+
+        return $this->_user;
     }
 }
